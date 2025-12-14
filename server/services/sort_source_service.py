f4475a1b5f265848bf7cb880f5f8c6270e375cad
Art0citus <mritik424@gmail.com>
2025-12-14 22:28:44 +0530

    sort source services

---
+++ server/services/sort_source_service.py
@@ -0,0 +1,9 @@
+from  typing import List
+from sentence_transformers import SentenceTransformer
+class SortSourceService:
+    def __init__(self):
+        self.embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+    def sort_sources(self, query: str, search_result: List[dict]):
+        query_embedding = self.embedding_model.encode(query)
+
+        print(query_embedding)
